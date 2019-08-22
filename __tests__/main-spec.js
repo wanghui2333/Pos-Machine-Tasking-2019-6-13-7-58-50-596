410c2f12d716ec4ce5e8e533a44fba76bab4345e
wanghui <793147654@qq.com>
2019-08-22 15:55:51 +0800

    done

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,5 +1,29 @@
-const add = require('../main');
+const { countProducts, fetchProduct } = require('../main');
 
-it ('should add two numbers', () => {
-    expect(add(2, 3)).toBe(5);
+it('should count codes', () => {
+    //given
+    const codes = ['0003', '0003', '0001']
+
+    //when
+    const result = countProducts(codes);
+    console.log(result);
+    //then
+    expect(result[0].count).toBe(2);
+});
+
+it('should fetch product', () => {
+    //given
+    const products = [
+        { code: '0003', count: 2 },
+        { code: '0001', count: 1 }
+    ];
+
+    //when
+    const result = fetchProduct(products);
+    console.log(result);
+    //then
+    expect(result[0].price).toBe(5);
+    expect(result[0].name).toBe("Pepsi-Cola");
+
+    expect(result[1].name).toBe("Coca Cola");
 });
--- main.js
+++ main.js
@@ -1,5 +1,53 @@
-function add (left, right) {
-    return left + right;
+const db = [
+    { "id": "0001", "name": "Coca Cola", "price": 3 },
+    { "id": "0002", "name": "Diet Coke", "price": 4 },
+    { "id": "0003", "name": "Pepsi-Cola", "price": 5 },
+    { "id": "0004", "name": "Mountain Dew", "price": 6 },
+    { "id": "0005", "name": "Dr Pepper", "price": 7 },
+    { "id": "0006", "name": "Sprite", "price": 8 },
+    { "id": "0007", "name": "Diet Pepsi", "price": 9 },
+    { "id": "0008", "name": "Diet Mountain Dew", "price": 10 },
+    { "id": "0009", "name": "Diet Dr Pepper", "price": 11 },
+    { "id": "0010", "name": "Fanta", "price": 12 }
+];
+
+function countProducts(codes) {
+
+    let map = new Map();
+    codes.forEach(element => {
+        if (map.has(element)) {
+            map.set(element, map.get(element) + 1);
+        } else {
+            map.set(element, 1);
+        }
+    });
+
+    let result = [];
+    map.forEach((value, key) => {
+        result.push({
+            code: key,
+            count: value
+        })
+    });
+    return result;
 }
 
-module.exports = add;
+function fetchProduct(products) {
+
+    let result = [];
+    products.forEach(product => {
+        db.forEach(element => {
+            if (element.id === product.code) {
+                product.price = element.price;
+                product.name = element.name;
+                result.push(product);
+                return false;
+            }
+        });
+    });
+    return result;
+}
+module.exports = {
+    countProducts,
+    fetchProduct
+};
